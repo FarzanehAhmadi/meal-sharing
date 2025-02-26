3ce53e6b7fc15a52e89e338004935d2c1062d6e8
FarzanehAhmadi <f_ahmadi74@yahoo.com>
2025-02-26 11:20:27 +0100

    Commented out unused routes and server setup

--- api/src/index.js
+++ api/src/index.js
@@ -46,15 +46,16 @@ apiRouter.get("/last-meal", async (req, res)=>{
   const lastMeal = meals.find((meal)=> meal.id === maxId)
   res.send(lastMeal)
 })
-// // You can delete this route once you add your own routes
-// apiRouter.get("/", async (req, res) => {
-//   const SHOW_TABLES_QUERY =
-//     process.env.DB_CLIENT === "pg"
-//       ? "SELECT * FROM pg_catalog.pg_tables;"
-//       : "SHOW TABLES;";
-//   const tables = await knex.raw(SHOW_TABLES_QUERY);
-//   res.json({ tables });
-// });
+/*
+// You can delete this route once you add your own routes
+apiRouter.get("/", async (req, res) => {
+  const SHOW_TABLES_QUERY =
+    process.env.DB_CLIENT === "pg"
+      ? "SELECT * FROM pg_catalog.pg_tables;"
+      : "SHOW TABLES;";
+  const tables = await knex.raw(SHOW_TABLES_QUERY);
+  res.json({ tables });
+});
 
 // This nested router example can also be replaced with your own sub-router
 apiRouter.use("/nested", nestedRouter);
@@ -64,3 +65,4 @@ app.use("/api", apiRouter);
 app.listen(process.env.PORT, () => {
   console.log(`API listening on port ${process.env.PORT}`);
 });
+*/
