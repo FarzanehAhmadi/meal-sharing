c82f0d016690c0c4f0b7a64c2fe6630dc630503f
FarzanehAhmadi <f_ahmadi74@yahoo.com>
2025-02-26 14:46:09 +0100

    Polish

--- api/src/index.js
+++ api/src/index.js
@@ -92,28 +92,26 @@ apiRouter.get("/last-meal", async (req, res)=>{
   
 })
 
-
-//Setup server
+//Server
 app.listen(3000, ()=>
   console.log("I am running..."))
-/*
-// You can delete this route once you add your own routes
-apiRouter.get("/", async (req, res) => {
-  const SHOW_TABLES_QUERY =
-    process.env.DB_CLIENT === "pg"
-      ? "SELECT * FROM pg_catalog.pg_tables;"
-      : "SHOW TABLES;";
-  const tables = await knex.raw(SHOW_TABLES_QUERY);
-  res.json({ tables });
-});
 
-// This nested router example can also be replaced with your own sub-router
-apiRouter.use("/nested", nestedRouter);
+// // You can delete this route once you add your own routes
+// apiRouter.get("/", async (req, res) => {
+//   const SHOW_TABLES_QUERY =
+//     process.env.DB_CLIENT === "pg"
+//       ? "SELECT * FROM pg_catalog.pg_tables;"
+//       : "SHOW TABLES;";
+//   const tables = await knex.raw(SHOW_TABLES_QUERY);
+//   res.json({ tables });
+// });
 
+// // This nested router example can also be replaced with your own sub-router
+// apiRouter.use("/nested", nestedRouter);
 
+ app.use("/api", apiRouter);
 
-app.listen(process.env.PORT, () => {
-  console.log(`API listening on port ${process.env.PORT}`);
-});
-*/
-app.use("/api", apiRouter);
+// app.listen(process.env.PORT, () => {
+//   console.log(`API listening on port ${process.env.PORT}`);
+// });
+
