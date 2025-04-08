18d55d5a25ba9bc2b4034ae05caf950ca0b94965
FarzanehAhmadi <f_ahmadi74@yahoo.com>
2025-04-08 12:05:53 +0200

    Implement CRUD operations for reviews

--- api/src/index.js
+++ api/src/index.js
@@ -2,10 +2,11 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import bodyParser from "body-parser";
-import knex from "./database_client.js";
+//import knex from "./database_client.js";
 import nestedRouter from "./routers/nested.js";
 import mealsRouter from "./routers/meals.js";
 import reservationRouter from "./routers/reservation.js";
+import reviewsRouter from "./routers/reviews.js";
 
 const app = express();
 app.use(cors());
@@ -13,11 +14,11 @@ app.use(bodyParser.json());
 
 const apiRouter = express.Router();
 
-
 // This nested router example can also be replaced with your own sub-router
 apiRouter.use("/nested", nestedRouter);
 apiRouter.use("/meals", mealsRouter);
 apiRouter.use("/reservations", reservationRouter);
+apiRouter.use("/reviews", reviewsRouter);
 
 app.use("/api", apiRouter);
 
